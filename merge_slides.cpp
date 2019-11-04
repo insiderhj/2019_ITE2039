dd893d56bffab3cb9d39f91b939b08262d0a5623
김희진 <insiderhj@gmail.com>
2019-11-04 23:47:12 +0900

    add documentations of merge_slides.cpp

--- merge_slides.cpp
+++ merge_slides.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <utility>
 
+// element_t: {TOTAL_COST, TOTAL_PAGES, SPLIT_INDEX}
 #define TOTAL_COST 0
 #define TOTAL_PAGES 1
 #define SPLIT_INDEX 2
@@ -10,33 +11,41 @@
 using std::vector;
 using std::string;
 
-typedef vector<int> element;
+typedef vector<int> element_t;
 
+// function redefinitions
+// base
 vector<string> split(string str);
 vector<int> str_to_int(vector<string> strs);
-vector< vector<element> > init_table(vector<int> nums);
+vector< vector<element_t> > init_table(vector<int> nums);
 
-void fill_table(vector< vector<element> >& v);
-element get_min_element(vector< vector<element> > v, int row, int col);
+// construct table
+void fill_table(vector< vector<element_t> >& v);
+element_t get_min_element(vector< vector<element_t> > v, int row, int col);
 
-string get_result(vector< vector<element> > table, int row, int col);
+// result
+string get_result(vector< vector<element_t> > table, int row, int col);
 
 int main() {
     string input, result;
     vector<string> strings;
     vector<int> numbers;
-    vector< vector<element> > table;
+    vector< vector<element_t> > table;
     
     while (true) {
         std::getline(std::cin, input);
+
+        // split by ' ' or '\t'
         strings = split(input);
 
+        // if empty input, terminate program
         if (strings.size() == 0) break;
 
         try {
             numbers = str_to_int(strings);
             table = init_table(numbers);
 
+            // check whether to print calculation order or not
             if (table.size() > 1) {
                 fill_table(table);
                 result = get_result(table, 0, table.size() - 1);
@@ -55,6 +64,12 @@ int main() {
     return 0;
 }
 
+/**
+ * split string by ' ' and '\t'.
+ * 
+ * @param str original string
+ * @return vector of strings splitted by ' ' and '\t'
+ */
 vector<string> split(string str) {
     vector<string> v;
     size_t start;
@@ -77,6 +92,14 @@ vector<string> split(string str) {
     return v;
 }
 
+/**
+ * change a vector of strings to a vector of ints.
+ * 
+ * @param strs vector of strings
+ * @return vector of ints converted from string
+ * @throw std::invalid_argument thrown by std::stoi
+ * @throw int 1 thrown when number is equal to or less then zero
+ */
 vector<int> str_to_int(vector<string> strs) {
     vector<int> v;
     int input;
@@ -90,17 +113,25 @@ vector<int> str_to_int(vector<string> strs) {
     return v;
 }
 
-vector< vector<element> > init_table(vector<int> nums) {
-    vector< vector<element> > v;
-    vector<element> tmp;
-    element e;
+/**
+ * initialize element table with total_cost 0
+ * 
+ * @param nums vector of input numbers
+ * @return 2d vector with initial settings
+ */
+vector< vector<element_t> > init_table(vector<int> nums) {
+    vector< vector<element_t> > v;
+    vector<element_t> tmp;
+    element_t e;
 
+    // initialize vector
     e.push_back(0);
     e.push_back(0);
     e.push_back(0);
-
     tmp.push_back(e);
+
     for (int i = 0; i < nums.size(); i++) {
+        // set total_pages of tmp then add to table
         tmp[0][TOTAL_PAGES] = nums[i];
         v.push_back(tmp);
     }
@@ -108,24 +139,35 @@ vector< vector<element> > init_table(vector<int> nums) {
     return v;
 }
 
-void fill_table(vector< vector<element> >& v) {
+/**
+ * fill table with elements.
+ * 
+ * @param v main table to fill in
+ */
+void fill_table(vector< vector<element_t> >& v) {
+    // fill in the table from 1 to v.size(), from bottom to top
+    // ex) if size() == 4, fill
+    //     [3, 1] -> [2, 1] -> [1, 1] -> [0, 1] ->
+    //     [2, 2] -> [1, 2] -> [0, 2] ->
+    //     [1, 3] -> [0, 3] ->
+    //     [0, 4]
     for (int i = 0; i < v.size() - 1; i++) {
-        element tmp;
-        tmp.push_back(v[i][0][TOTAL_PAGES] + v[i + 1][0][TOTAL_PAGES]);
-        tmp.push_back(v[i][0][TOTAL_PAGES] + v[i + 1][0][TOTAL_PAGES]);
-        tmp.push_back(0);
-        v[i].push_back(tmp);
-    }
-
-    for (int i = 0; i < v.size() - 2; i++) {
         for (int j = i; j >= 0; j--) {
-            v[j].push_back(get_min_element(v, j, i + 2 - j));
+            v[j].push_back(get_min_element(v, j, i + 1 - j));
         }
     }
 }
 
-element get_min_element(vector< vector<element> > v, int row, int col) {
-    element res;
+/**
+ * get minimum total_cost that can fit in v[row][col].
+ *
+ * @param v container vector of elements
+ * @param row row number of element
+ * @param col column number of element
+ * @return vector containing minimum total_cost with total_pages and split_index
+ */
+element_t get_min_element(vector< vector<element_t> > v, int row, int col) {
+    element_t res;
     res.push_back(INT_MAX);
     res.push_back(v[row][0][TOTAL_PAGES] + v[row + 1][col - 1][TOTAL_PAGES]);
     res.push_back(0);
@@ -142,7 +184,15 @@ element get_min_element(vector< vector<element> > v, int row, int col) {
     return res;
 }
 
-string get_result(vector< vector<element> > table, int row, int col) {
+/**
+ * get result string which shows calculation order.
+ * 
+ * @param table main table with elements
+ * @param row row number of result
+ * @param col column number of result
+ * @return numbers enclosed in parentheses by calculation order
+ */
+string get_result(vector< vector<element_t> > table, int row, int col) {
     string res;
     if (col == 0) {
         return std::to_string(table[row][col][TOTAL_PAGES]);
